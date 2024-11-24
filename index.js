b0fb01181a3d23adaec4b45f8505b9a6541c6960
mmedoo <mmedoo.active@gmail.com>
2024-11-24 17:58:05 +0200

    Comment out unused middleware and route handling in index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const app = express();
-const path = require("path");
-const logger = require("morgan");
+// const path = require("path");
+// const logger = require("morgan");
+
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
-const cors = require("cors");
-const routers = require("./routers/all");
+
+
+// const cors = require("cors");
+// const routers = require("./routers/all");
 
 const swaggerDocument = YAML.load("./swagger.yaml");
 
@@ -16,13 +19,14 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger("dev"));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
-for (let route of Object.values(routers)) {
-	app.use('/', route);
-}
+// app.use(logger("dev"));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(cors());
+
+// for (let route of Object.values(routers)) {
+	// app.use('/', route);
+// }
 
 const port = process.env.PORT || 3300;
 
